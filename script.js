1afeacd61ca3bd2347772e569380dc7f00d632b1
Olukoya <alexolukoya@my.whccd.edu>
2020-02-05 19:58:46 -0800

    i wrote a function that takes the radius of a circle and outputs it appropriate circumference

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const min = function(min1, min2,) {
-    if (min1 < min2)
+const min = function(min1, min2) {
+    if (min1 <= min2)
     {
-        min3 = min1
-    }
-
-    return  min1;
+        return min1;
+    }    
+        return min2;
 };
-console.log(min(5, 98)); // should output 5
+console.log(min(5, 98, 27)); // should output 5
 /////////////////////////////////////////////////
+const calculatecircumference = function(radius) {
+let pi = 3.14;
+return 2 * pi * radius
+};
+console.log(calculatecircumference(5));
