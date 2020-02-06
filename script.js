6633731c2ea0f3725bb87deae8d93db64e965823
Olukoya <alexolukoya@my.whccd.edu>
2020-02-05 21:09:31 -0800

    i wrote a function in search string

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const min = function(min1, min2) {
         return min2;
 };
 console.log(min(5, 98, 27)); // should output 5
-/////////////////////////////////////////////////
+
 const calculatecircumference = function(radius) {
 let pi = 3.14;
 return 2 * pi * radius
 };
-console.log(calculatecircumference(5));
+console.log(calculatecircumference(5))
+
+const searchString = (letterToSearchFor, wordToSearch) => {
+let numberOfCharactersFound = 0;
+for(let i = 0; i <= wordToSearch.length -1; i++) {
+if(wordToSearch[i] == letterToSearchFor)
+{
+    numberOfCharactersFound++;
+}
+}
+return numberOfCharactersFound;
+};
+console.log(searchString("s", "mississippi"));
