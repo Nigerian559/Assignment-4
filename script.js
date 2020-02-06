2eee3936cf49ce2da900a5867efe8c01f7ec5166
Olukoya <alexolukoya@my.whccd.edu>
2020-02-05 19:19:55 -0800

    i wrote a function that takes two numbers and returns the smallest pair

---
+++ script.js
@@ -0,0 +1,10 @@
+const min = function(min1, min2,) {
+    if (min1 < min2)
+    {
+        min3 = min1
+    }
+
+    return  min1;
+};
+console.log(min(5, 98)); // should output 5
+/////////////////////////////////////////////////
